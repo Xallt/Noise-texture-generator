843b33282734392d60019d90b3f8bfc92553e122
Dmitriy Shabat <mitya.shabat@gmail.com>
2019-08-10 00:23:46 +0300

    Little css fix

--- src/components/WebsiteCaption.js
+++ src/components/WebsiteCaption.js
@@ -3,7 +3,7 @@ import React from 'react'
 class Caption extends React.Component {
   render() {
     return (<>
-      <h1 align='center'>Noise generation</h1>
+      <h1 align='center' style={{margin:"2% 0 0 0"}}>Noise generation</h1>
       <h4 align='center'>Made by <a href="https://xallt.github.io">Xallt</a></h4>
     </>)
   }
