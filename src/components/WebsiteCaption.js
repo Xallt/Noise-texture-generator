81afc414f33134d8f6109533fef1d20244316794
Dmitriy Shabat <mitya.shabat@gmail.com>
2019-08-11 16:34:40 +0300

    Changed the caption

--- src/components/WebsiteCaption.js
+++ src/components/WebsiteCaption.js
@@ -7,7 +7,7 @@ class Caption extends React.Component {
       <div ref={(el) => {this.el = el}} className="fill text-autosize">
         <h1 align='center' style={{margin:"4.5% 0 0 0"}}>
           Noise generation</h1>
-        <h4 align='center'>Made by <a href="https://xallt.github.io">Xallt</a></h4>
+        <h4 align='center'>See on <a href="https://github.com/Xallt/Noise-map-generator">Github</a></h4>
       </div>
     )
   }
